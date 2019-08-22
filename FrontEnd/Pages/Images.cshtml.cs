e9ae6a46e02b39351de5a8b569b6a179bc53b68b
shonneh <nenadmisic994@gmail.com>
2019-08-22 19:36:32 +0200

    Add image resize using ImageSharp

--- FrontEnd/Pages/Images.cshtml.cs
+++ FrontEnd/Pages/Images.cshtml.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using ConferenceDTO;
 using FrontEnd.Pages.Models;
 using FrontEnd.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using Image = SixLabors.ImageSharp.Image;
 
 namespace FrontEnd.Pages
 {
@@ -26,20 +34,51 @@ namespace FrontEnd.Pages
         [BindProperty]
         public ImageUpload ImageUpload { get; set; }
 
-        public string ImageUrl { get; set; }
-
-        public IEnumerable<ImageResponse> Images { get; set; }
+        public IEnumerable<Models.Image> Images { get; set; }
 
         public async Task OnGetAsync()
         {
-            Images = await _apiClient.GetImagesAsync();
+            var images = await _apiClient.GetImagesAsync();
 
-            var first = Images.FirstOrDefault();
+            Images = images.Where(i => i.ImageType == "image/png" || i.ImageType == "image/jpeg")
+                           .Select(ConvertToImages());
+        }
 
-            if (first != null)
+        private static Func<ImageResponse, Models.Image> ConvertToImages()
+        {
+            return image =>
             {
-                ImageUrl = string.Format("data:/image" + first.ImageType + ";base64,{0}", first.Content);
-            }
+                using (var ms = new MemoryStream(Convert.FromBase64String(image.Content)))
+                {
+                    IImageDecoder decoder = new PngDecoder();
+                    if (image.ImageType == "image/jpeg")
+                    {
+                        decoder = new JpegDecoder();
+                    }
+                    using (Image<Rgba32> imageSharp = Image.Load(ms, decoder))
+                    {
+                        imageSharp.Mutate(ctx => ctx.Resize(imageSharp.Width / 2, imageSharp.Height / 2));
+
+                        using (var outputStream = new MemoryStream())
+                        {
+                            IImageEncoder encoder = new PngEncoder();
+                            if (image.ImageType == "image/jpeg")
+                            {
+                                encoder = new JpegEncoder();
+                            }
+                            imageSharp.Save(outputStream, encoder);
+                            outputStream.Position = 0;
+                            var content = Convert.ToBase64String(outputStream.ToArray());
+
+                            return new Models.Image
+                            {
+                                ImageUrl = string.Format("data:/" + image.ImageType + ";base64,{0}", content),
+                                Name = image.Name
+                            };
+                        }
+                    }
+                }
+            };
         }
 
         public async Task<IActionResult> OnPostAsync()
@@ -52,13 +91,15 @@ namespace FrontEnd.Pages
             using (var ms = new MemoryStream())
             {
                 var image = ImageUpload.Image;
+
                 await image.CopyToAsync(ms);
+
                 var imageBytes = ms.ToArray();
                 var imageContent = Convert.ToBase64String(imageBytes);
 
                 var imageRequest = new ImageRequest
                 {
-                    Name = image.Name,
+                    Name = ImageUpload.Name,
                     Content = imageContent,
                     ImageType = image.ContentType
                 };
---
+++ FrontEnd/Pages/Models/Image.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel;
+
+namespace FrontEnd.Pages.Models
+{
+    public class Image : ConferenceDTO.Image
+    {
+        public string ImageUrl { get; set; }
+    }
+}
